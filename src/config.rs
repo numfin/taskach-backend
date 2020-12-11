7e69d2f2d4a743f74af7e9cbbcd9008aaed8b7fb
numfin <numfin@yandex.ru>
2020-12-11 20:00:21 +0300

    healthcheck route

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,8 @@
 use crate::graphql;
-use actix_web::web;
+use actix_web::{web, HttpResponse};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/graphql").route(web::post().to(graphql::handlers::graphql)))
+    cfg.service(web::resource("/").route(web::get().to(|| HttpResponse::Ok())))
+        .service(web::resource("/graphql").route(web::post().to(graphql::handlers::graphql)))
         .service(web::resource("/graphiql").route(web::get().to(graphql::handlers::graphiql)));
 }
