94dab3693dfea06c20673c91e57a5f41124722a0
numfin <numfin@yandex.ru>
2020-12-31 02:59:05 +0300

    turning on queries and mutations

--- src/graphql/mutations.rs
+++ src/graphql/mutations.rs
@@ -1,6 +1,6 @@
 use super::Context;
-// use crate::projects::mutations::MutationProjects;
-// use crate::stories::mutations::MutationStories;
+use crate::projects::mutations::MutationProjects;
+use crate::stories::mutations::MutationStories;
 use crate::users::mutations::MutationUsers;
 
 pub struct MutationRoot;
@@ -9,10 +9,10 @@ impl MutationRoot {
     fn users(&self) -> MutationUsers {
         MutationUsers
     }
-    // fn projects(&self) -> MutationProjects {
-    //     MutationProjects
-    // }
-    // fn stories(&self) -> MutationStories {
-    //     MutationStories
-    // }
+    fn projects(&self) -> MutationProjects {
+        MutationProjects
+    }
+    fn stories(&self) -> MutationStories {
+        MutationStories
+    }
 }
--- src/graphql/queries.rs
+++ src/graphql/queries.rs
@@ -1,6 +1,6 @@
 use super::Context;
-// use crate::projects::queries::QueryProjects;
-// use crate::stories::queries::QueryStories;
+use crate::projects::queries::QueryProjects;
+use crate::stories::queries::QueryStories;
 use crate::users::queries::QueryUsers;
 
 pub struct QueryRoot;
@@ -11,10 +11,10 @@ impl QueryRoot {
     fn users(&self) -> QueryUsers {
         QueryUsers
     }
-    // fn projects(&self) -> QueryProjects {
-    //     QueryProjects
-    // }
-    // fn stories(&self) -> QueryStories {
-    //     QueryStories
-    // }
+    fn projects(&self) -> QueryProjects {
+        QueryProjects
+    }
+    fn stories(&self) -> QueryStories {
+        QueryStories
+    }
 }
